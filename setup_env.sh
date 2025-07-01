654f1ee5cc580858e25f16a154a938ac285b3e8d
Fengze Yu <fy2243@nyu.edu>
2025-07-01 13:24:21 -0400

    use c++ 14 to compatible with Pangolin
    Signed-off-by: Fengze Yu <fy2243@nyu.edu>

--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Setup environment for ORB-SLAM3 with local OpenCV
+# Setup environment for ORB-SLAM3 with local OpenCV and Pangolin
 
 # Activate Python virtual environment
 source ~/venv/py39/bin/activate
@@ -9,8 +9,14 @@ export OpenCV_DIR=$HOME/venv/py39/opencv_cpp/lib64/cmake/opencv4
 export PKG_CONFIG_PATH=$HOME/venv/py39/opencv_cpp/lib64/pkgconfig:$PKG_CONFIG_PATH
 export LD_LIBRARY_PATH=$HOME/venv/py39/opencv_cpp/lib64:$LD_LIBRARY_PATH
 
+# Set Pangolin paths
+export Pangolin_DIR=$HOME/venv/py39/pangolin/lib/cmake/Pangolin
+export CMAKE_PREFIX_PATH=$HOME/venv/py39/pangolin:$CMAKE_PREFIX_PATH
+export LD_LIBRARY_PATH=$HOME/venv/py39/pangolin/lib:$LD_LIBRARY_PATH
+
 echo "Environment configured:"
 echo "  Python venv: $VIRTUAL_ENV"
 echo "  OpenCV_DIR: $OpenCV_DIR"
+echo "  Pangolin_DIR: $Pangolin_DIR"
 echo ""
 echo "You can now build ORB-SLAM3 with: ./build.sh"
